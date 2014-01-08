e601a206336da2a52adbdbf17c0eaa50ddcd70b3
Omar Diab <o.s.diab@gmail.com>
2014-01-08 13:41:58 -0800

    Tried out default args in functions

---
+++ 02-default-args.py
@@ -0,0 +1,9 @@
+def foo(arg1="hello", arg2="world"):
+    print "{} {}".format(arg1, arg2)
+
+foo()
+foo('haldo', 'waldo')
+foo(arg2='willy', arg1='hilly')
+foo(arg1='heyy')
+foo(arg2='fool')
+foo('herro')
